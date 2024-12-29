44e535c7bf15d6608e5581c635c2b8ae75f23b53
Olga <olga@Stans-Macbook.local>
2024-12-28 21:13:56 -0500

    Try 11

--- vite.config.ts
+++ vite.config.ts
@@ -2,17 +2,5 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          vendor: ['react', 'react-dom', 'lucide-react'],
-        },
-      },
-    },
-  },
+  plugins: [react()]
 });
